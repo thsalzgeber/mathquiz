90baaf1fc9f54be3e0e63caa65124364f787aef9
Thomas <th.salzgeber@gmail.com>
2024-03-31 00:06:32 -0700

    create nav menu

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,31 @@
 document.getElementById("header-title").innerHTML = "Math Quiz";
 
 // Icons
-const pathToIcons = "icons/";
+const textTitle = document.querySelector('title').innerText.toLowerCase();
+const title = textTitle.slice(textTitle.indexOf('-') + 2, textTitle.length);
+const pathToIcons = title === "page not found" ? "/jsprojects/icons/" : "icons/";
 document.querySelector('.home-image').innerHTML = `<img src="${pathToIcons}home-18.png"> Home`;
-document.querySelector('.contact-email-address').innerHTML = `<img src="${pathToIcons}email-18.png"> <a href="mailto:thomas@stnet.at"> thomas@stnet.at</a>`;
+document.querySelector('.game-image').innerHTML = `<img src="${pathToIcons}game-18.png"> Games`;
+document.querySelector('.quiz-image').innerHTML = `<img src="${pathToIcons}quiz-18.png"> Quizzes`;
 document.querySelector('.footer-top').innerHTML = `<img src="${pathToIcons}arrow-up-18.png">  [ Top ]`;
 
 
 // Impressum Text
-document.getElementById("impressum-date").innerHTML = "&copy April 2, 2024";
+document.getElementById("impressum-date").innerHTML = "&copy April 2024";
 document.getElementById("impressum-name").innerHTML = "Thomas Salzgeber";
+
+// Email address
+const emailAddress = "\u0074\u0068\u006f\u006d\u0061\u0073\u0040\u0077\u0065\u0062\u0064\u0065\u0076\u002e\u0073\u0074\u006e\u0065\u0074\u002e\u0061\u0074";
+
+function link(text, href) {
+    let a = document.createElement("a");
+    a.textContent = text;
+    a.href = href;
+    if (!text.includes("@")) {
+        a.target = "_blank";
+    }
+    return a;
+}
+
+document.querySelector('.email-symbol').innerHTML = `<img src="${pathToIcons}email-18.png"> `;
+document.querySelector(".contact-email-address").appendChild(link(emailAddress, `mailto:${emailAddress}`));
--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,16 @@ const sectionHeader = document.getElementById('section-header').innerHTML = "Hav
 const sectionHangman = document.getElementById('math-quiz').innerHTML = "Choose a Math Quiz: ";
 const sectionMathQuiz = document.getElementById('statistic').innerHTML = "Statistic";
 
+// Navigation Menu
+const dropDowns = document.getElementsByClassName("sub-nav");
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown')) {
+        for (var i = 0; i < dropDowns.length; i++) {
+            var openDropdown = dropDowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
