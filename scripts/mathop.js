4d7086c3978e88ee9455e02c3536bb2371592c05
Thomas <th.salzgeber@gmail.com>
2024-04-02 21:46:01 -0700

    math operations

---
+++ scripts/mathop.js
@@ -0,0 +1,73 @@
+class Mathop {
+    constructor(number1, number2, input, operation) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.input = input;
+        this.operation = operation;
+    }
+
+    mathOperation() {
+        switch (this.operation) {
+            case "*":
+                return calcMultiplication(this.number1, this.number2, this.input);
+            case "/":
+                return calcDivision(this.number1, this.number2, this.input);
+            case "%":
+                return calcDivisionRemainer(this.number1, this.number2, this.input);
+            case "/r":
+                return calcDivisionRounded(this.number1, this.number2, this.input);
+            case "+":
+                return calcAddition(this.number1, this.number2, this.input);
+            case "-":
+                return calcSubstraction(this.number1, this.number2, this.input);
+            default:
+                console.log("this is the end");
+        }
+    }
+
+}
+
+function calcMultiplication(n1, n2, input) {
+    return {
+        result: n1 * n2,
+        resultBoolean: +input === n1 * n2 ? true : false,
+    }
+}
+
+function calcDivision(n1, n2, input) {
+    return {
+        result: n1 / n2,
+        resultBoolean: +input === n1 / n2 ? true : false,
+    }
+}
+
+function calcDivisionRemainer(n1, n2, input) {
+    const remainerResult = (Math.trunc(n1 / n2)) + "r" + (n1 % n2);
+    return {
+        result: remainerResult,
+        resultBoolean: input === remainerResult ? true : false,
+    }
+}
+
+function calcDivisionRounded(n1, n2, input) {
+    return {
+        result: parseFloat((n1 / n2).toFixed(2)),
+        resultBoolean: +input === parseFloat((n1 / n2).toFixed(2)) ? true : false,
+    }
+}
+
+function calcAddition(n1, n2, input) {
+    return {
+        result: n1 + n2,
+        resultBoolean: +input === n1 + n2 ? true : false,
+    }
+}
+
+function calcSubstraction(n1, n2, input) {
+    return {
+        result: n1 - n2,
+        resultBoolean: +input === (n1 - n2) ? true : false,
+    }
+}
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -5,34 +5,74 @@ const sectionMathQuiz = document.getElementById('statistic').innerHTML = "Statis
 
 const $mathChoice = $('input[name="mathchoice"]');
 const $mathOperation = $('#mathoperation');
+const $inputResult = $('#input-result');
+const $okButton = $('#ok-btn');
+const $result = $('#result');
+const $resultBoolean = $('#result-boolean');
 
-let n1 = 5;
-let n2 = 3;
-let op = "*";
+let n1 = 0;
+let n2 = 0;
+let op = strOp = "*";
 
 init();
 
 function init() {
-    $mathOperation.text(`${n1} ${op} ${n2} = `);
+    createMath();
+
 }
 
-$mathChoice.click(function () {
-    op = mathSelection();
-    console.log(op);
-    $mathOperation.text(`${n1} ${op} ${n2} = `);
+$okButton.click(function () {
+    verifyUserInput();
+    $inputResult.val("");
+    $inputResult.focus();
+
 });
 
-// $mathOperation.text(`${n1} ${op} ${n2} = `);
+$mathChoice.click(function () {
+    op = strOp = mathSelection();
+    createMath();
+});
+
+function createMath() {
+    n1 = randNum();
+    n2 = randNum();
+
+    if (n1 < n2) [n1, n2] = [n2, n1];
+
+    n1 = op === "-" ? (n1 + n2) : n1;
+    n1 = op === "/" ? (n1 * n2) : n1;
+
+    if (op === "%" || op === "/r") strOp = "/";
+
+    $mathOperation.text(`${n1} ${strOp} ${n2} = `);
+}
+
+function randNum() {
+    // return Math.floor(Math.random() * 99) + 2;
+    return Math.floor(Math.random() * 9) + 2;
+}
+
+function verifyUserInput() {
+    const mathCalculation = new Mathop(n1, n2, $inputResult.val(), op);
+    // console.log(mathCalculation.mathOperation().result, mathCalculation.mathOperation().resultBoolean);
+    $result.text(mathCalculation.mathOperation().result);
+    $resultBoolean.text(mathCalculation.mathOperation().resultBoolean);
+    createMath();
+
+}
 
 function mathSelection() {
     for (const radioButton of $mathChoice) {
         if (radioButton.checked) if (radioButton.value === 'math-1') return "*";
         if (radioButton.checked) if (radioButton.value === 'math-2') return "/";
-        if (radioButton.checked) if (radioButton.value === 'math-3') return "/";
-        if (radioButton.checked) if (radioButton.value === 'math-4') return "/";
+        if (radioButton.checked) if (radioButton.value === 'math-3') return "%";
+        if (radioButton.checked) if (radioButton.value === 'math-4') return "/r";
         if (radioButton.checked) if (radioButton.value === 'math-5') return "+";
         if (radioButton.checked) if (radioButton.value === 'math-6') return "-";
         if (radioButton.checked) if (radioButton.value === 'math-7') return 0;
     }
+}
 
+function checInput(userInput) {
+    const numbersOnly = /[0-9.]+/;
 }
